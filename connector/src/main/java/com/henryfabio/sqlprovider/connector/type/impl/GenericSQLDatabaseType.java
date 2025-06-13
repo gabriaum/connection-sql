e1a7300f7603f723c88024c3ad0c8e92ef822bf3
Your Name <you@example.com>
2025-06-13 01:00:06 -0300

    enabling multi-compatibility with SQL databases

---
+++ connector/src/main/java/com/henryfabio/sqlprovider/connector/type/impl/GenericSQLDatabaseType.java
@@ -0,0 +1,22 @@
+package com.henryfabio.sqlprovider.connector.type.impl;
+
+import com.henryfabio.sqlprovider.connector.type.RemoteDatabaseType;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public final class GenericSQLDatabaseType extends RemoteDatabaseType {
+
+    /**
+     * @param driverClass e.g. com.mysql.jdbc.Driver, org.postgresql.Driver
+     * @param jdbcUrl Example jdbc:mysql://localhost/db, jdbc:postgresql://localhost/db
+     * @param username e.g. root, postgres
+     * @param password e.g. empty string, postgres
+     */
+    public GenericSQLDatabaseType(String driverClass, String jdbcUrl, String username, String password) {
+        super(driverClass, jdbcUrl,
+                username != null ? username : "root",
+                password != null ? password : "");
+    }
+}
